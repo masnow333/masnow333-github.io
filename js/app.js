43a2bf85ba7ef8949547eb9ce5125b75a1989645
Jonathan Triana <jonfertri357@gmail.com>
2020-08-07 17:59:07 -0500

    The problem with the menu, was solved

--- js/app.js
+++ js/app.js
@@ -2,8 +2,15 @@ const imageHover = document.querySelectorAll('div.mini > img');
 const imageMove = document.querySelector('div.pictureShow > div');
 const proyectsWrapper = document.querySelector('div.projectsShow > div.wrapper');
 const responsiveButton = document.querySelector('nav > ul > div.span');
+const menuButtons = document.querySelectorAll('nav > ul > li > a');
 const nav = document.querySelector('nav');
-console.log(nav);
+console.log(menuButtons);
+
+menuButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        nav.classList.remove('visibility');
+    });
+});
 
 responsiveButton.addEventListener('click', () => {
     nav.classList.toggle('visibility');

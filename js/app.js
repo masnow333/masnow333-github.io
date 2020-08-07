92ce3aac8ac892c4bd1d324a1abf4cdb878f9034
Jonathan Triana <jonfertri357@gmail.com>
2020-08-07 16:00:34 -0500

    The responsive was added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 const imageHover = document.querySelectorAll('div.mini > img');
 const imageMove = document.querySelector('div.pictureShow > div');
 const proyectsWrapper = document.querySelector('div.projectsShow > div.wrapper');
+const responsiveButton = document.querySelector('nav > ul > div.span');
+const nav = document.querySelector('nav');
+console.log(nav);
+
+responsiveButton.addEventListener('click', () => {
+    nav.classList.toggle('visibility');
+});
 
 imageHover.forEach((image, i) => {
     image.addEventListener('mouseover', () => {
